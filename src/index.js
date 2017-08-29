537490b45499e2ad6eeb9aa87c2ef8ee4a0590fa
Parker Burnett <burnettparker@gmail.com>
2017-08-28 22:48:47 -0600

    Initial commit with all the source files
    Initial commit with the source files, the index.js, the recipes.js and other alexa intents and files.

---
+++ src/index.js
@@ -0,0 +1,105 @@
+'use strict';
+
+var Alexa = require('alexa-sdk');
+var APP_ID = 'amzn1.ask.skill.21c971fb-5ee8-4918-b366-e4443799ec99'; // App ID
+var recipes = require('./recipes');
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    // To enable string internationalization (i18n) features, set a resources object.
+    alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    //Use LaunchRequest, instead of NewSession if you want to use the one-shot model
+    // Alexa, ask [my-skill-invocation-name] to (do something)...
+    'LaunchRequest': function () {
+        this.attributes['speechOutput'] = this.t("WELCOME_MESSAGE", this.t("SKILL_NAME"));
+        // If the user does not reply to the welcome message or says something that is not
+        // understood, they will be prompted again with a message.
+        this.attributes['repromptSpeech'] = this.t("WELCOME_REPROMPT");
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+    },
+    'RecipeIntent': function () {
+        var itemSlot = this.event.request.intent.slots.Item;
+        var itemName;
+        if (itemSlot && itemSlot.value) {
+            itemName = itemSlot.value.toLowerCase();
+        }
+
+        var cardTitle = this.t("DISPLAY_CARD_TITLE", this.t("SKILL_NAME"), itemName);
+        var recipes = this.t("RECIPES");
+        var recipe = recipes[itemName];
+
+        if (recipe) {
+            this.attributes['speechOutput'] = recipe;
+            this.attributes['repromptSpeech'] = this.t("RECIPE_REPEAT_MESSAGE");
+            this.emit(':tellWithCard', recipe, this.attributes['repromptSpeech'], cardTitle, recipe);
+        } else {
+            var speechOutput = this.t("RECIPE_NOT_FOUND_MESSAGE");
+            var repromptSpeech = this.t("RECIPE_NOT_FOUND_REPROMPT");
+            if (itemName) {
+                speechOutput += this.t("RECIPE_NOT_FOUND_WITH_ITEM_NAME", itemName);
+            } else {
+                speechOutput += this.t("RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME");
+            }
+            speechOutput += repromptSpeech;
+
+            this.attributes['speechOutput'] = speechOutput;
+            this.attributes['repromptSpeech'] = repromptSpeech;
+
+            this.emit(':ask', speechOutput, repromptSpeech);
+        }
+    },
+    'AMAZON.HelpIntent': function () {
+        this.attributes['speechOutput'] = this.t("HELP_MESSAGE");
+        this.attributes['repromptSpeech'] = this.t("HELP_REPROMPT");
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+    },
+    'AMAZON.RepeatIntent': function () {
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit('SessionEndedRequest');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit('SessionEndedRequest');
+    },
+    'SessionEndedRequest':function () {
+        this.emit(':tell', this.t("STOP_MESSAGE"));
+    },
+    'Unhandled': function () {
+        this.attributes['speechOutput'] = this.t("HELP_MESSAGE");
+        this.attributes['repromptSpeech'] = this.t("HELP_REPROMPT");
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+    }
+};
+
+var languageStrings = {
+    "en": {
+        "translation": {
+            "RECIPES": recipes.RECIPE_EN_US,
+            "SKILL_NAME": "Terraria Crafting Tool",
+            "WELCOME_MESSAGE": "Welcome to %s. You can ask a question like, what\'s the recipe for a torch? ... So, what can I help you craft.",
+            "WELCOME_REPROMPT": "For instructions on what you can say, please say help me.",
+            "DISPLAY_CARD_TITLE": "%s  - Recipe for %s.",
+            "HELP_MESSAGE": "You can ask questions such as, what\'s the recipe, or, you can say exit...So, what can I help you with?",
+            "HELP_REPROMPT": "You can say things like, what\'s the recipe, or you can say exit...So how can I help you?",
+            "STOP_MESSAGE": "Goodbye!",
+            "RECIPE_REPEAT_MESSAGE": "Try saying repeat.",
+            "RECIPE_NOT_FOUND_MESSAGE": "I\'m sorry, I currently do not know, try back later ",
+            "RECIPE_NOT_FOUND_WITH_ITEM_NAME": "the recipe for %s. ",
+            "RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME": "that recipe. ",
+            "RECIPE_NOT_FOUND_REPROMPT": "What else can I help with?"
+        }
+    },
+    "en-US": {
+        "translation": {
+            "RECIPES" : recipes.RECIPE_EN_US,
+            "SKILL_NAME" : "American Terraria Crafting Tool"
+        }
+    }
+};
---
+++ src/recipes.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "RECIPE_EN_US" : {
+        "slime": "slime can be obtained by killing slime enemies.",
+        "wood": "Wood can be obtained by chopping down a tree with an axe.",
+        "torch": "A torch can be crafted with one gel and one wood",
+        "furnace": "A furnace can be crafted by using twenty stone blocks, four wood, and three torches",
+        "work bench": "A work bench can be made with ten wood",
+        "iron bar": "An iron bar can be made using the furnace to smelt three iron ore",
+        "gold bar": "A gold bar can be made using the furnace to smelt three gold ore",
+        "copper bar": "A copper bar can be made using the furnace to smelt three copper ore",
+        "grey brick": "A grey brick can be made by using 2 stone at a furnace",
+        "bottle": "A bottle can be made by using 1 glass block at a furnace",
+        "tin bar": "A tin bar can be made using the furnace to smelt three tin ore"
+    }
+};
