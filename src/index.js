0c06111ebafb8d846788203f11e4bd3520d6c25d
Parker Burnett <burnettparker@gmail.com>
2017-08-30 14:28:39 -0600

    Updating the files
    Updating the files to the most recent version after reading more of the Alexa documentation.

--- src/index.js
+++ src/index.js
@@ -1,66 +1,94 @@
+/**
+ * This Skill demonstrates how to craft certain items in the computer game Terraria
+ * This supports multiple languages (en-US, en-GB, more to come soon, I know there isn't any difference between the english).
+ * The Intents, Slot and Sample Utterances for this skill, as well as the code
+ * and instructions are located at https://github.com/jpburnett/terraria-tool-skill
+ **/
+
 'use strict';
 
-var Alexa = require('alexa-sdk');
-var APP_ID = 'amzn1.ask.skill.21c971fb-5ee8-4918-b366-e4443799ec99'; // App ID
-var recipes = require('./recipes');
+const Alexa = require('alexa-sdk');
+const recipes = require('./recipes');
 
-exports.handler = function(event, context, callback) {
-    var alexa = Alexa.handler(event, context);
-    alexa.APP_ID = APP_ID;
-    // To enable string internationalization (i18n) features, set a resources object.
-    alexa.resources = languageStrings;
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+const APP_ID = "amzn1.ask.skill.aef9004a-f2bc-42a6-bd57-d01742f264e5";
+
+const languageStrings = {
+    'en': {
+        translation: {
+            RECIPES: recipes.RECIPE_EN_US,
+            SKILL_NAME: 'Terraria Tool',
+            WELCOME_MESSAGE: "Welcome to %s. You can ask a question like, what\'s the recipe for a torch? ... Now, how may I help you?",
+            WELCOME_REPROMPT: 'For instructions on what you can say, please say help me.',
+            DISPLAY_CARD_TITLE: '%s  - Recipe for %s.',
+            HELP_MESSAGE: "You can ask questions such as, what\'s the recipe, or, you can say exit...Now, how may I help you?",
+            HELP_REPROMPT: "You can say things like, what\'s the recipe, or you can say exit...Now, how may I help you?",
+            STOP_MESSAGE: 'Happy Crafting!',
+            RECIPE_REPEAT_MESSAGE: 'Try saying repeat.',
+            RECIPE_NOT_FOUND_MESSAGE: "I\'m sorry, I don\'t know that recipe yet.",
+            RECIPE_NOT_FOUND_WITH_ITEM_NAME: 'the recipe for %s. ',
+            RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME: 'that recipe. ',
+            RECIPE_NOT_FOUND_REPROMPT: 'Is there anything else can I help with?',
+        },
+    },
+    'en-US': {
+        translation: {
+            RECIPES: recipes.RECIPE_EN_US,
+            SKILL_NAME: 'American Terraria Tool',
+        },
+    },
+    'en-GB': {
+        translation: {
+            RECIPES: recipes.RECIPE_EN_GB,
+            SKILL_NAME: 'British Terraria Tool',
+        },
+    },
 };
 
-var handlers = {
-    //Use LaunchRequest, instead of NewSession if you want to use the one-shot model
-    // Alexa, ask [my-skill-invocation-name] to (do something)...
+const handlers = {
     'LaunchRequest': function () {
-        this.attributes['speechOutput'] = this.t("WELCOME_MESSAGE", this.t("SKILL_NAME"));
-        // If the user does not reply to the welcome message or says something that is not
-        // understood, they will be prompted again with a message.
-        this.attributes['repromptSpeech'] = this.t("WELCOME_REPROMPT");
-        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+        this.attributes.speechOutput = this.t('WELCOME_MESSAGE', this.t('SKILL_NAME'));
+        // If the user does not reply to the welcome message or says something that is not understood, they will be prompted again with this text.
+        this.attributes.repromptSpeech = this.t('WELCOME_REPROMPT');
+        this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
     },
     'RecipeIntent': function () {
-        var itemSlot = this.event.request.intent.slots.Item;
-        var itemName;
+        const itemSlot = this.event.request.intent.slots.Item;
+        let itemName;
         if (itemSlot && itemSlot.value) {
             itemName = itemSlot.value.toLowerCase();
         }
 
-        var cardTitle = this.t("DISPLAY_CARD_TITLE", this.t("SKILL_NAME"), itemName);
-        var recipes = this.t("RECIPES");
-        var recipe = recipes[itemName];
+        const cardTitle = this.t('DISPLAY_CARD_TITLE', this.t('SKILL_NAME'), itemName);
+        const myRecipes = this.t('RECIPES');
+        const recipe = myRecipes[itemName];
 
         if (recipe) {
-            this.attributes['speechOutput'] = recipe;
-            this.attributes['repromptSpeech'] = this.t("RECIPE_REPEAT_MESSAGE");
-            this.emit(':tellWithCard', recipe, this.attributes['repromptSpeech'], cardTitle, recipe);
+            this.attributes.speechOutput = recipe;
+            this.attributes.repromptSpeech = this.t('RECIPE_REPEAT_MESSAGE');
+            this.emit(':askWithCard', recipe, this.attributes.repromptSpeech, cardTitle, recipe);
         } else {
-            var speechOutput = this.t("RECIPE_NOT_FOUND_MESSAGE");
-            var repromptSpeech = this.t("RECIPE_NOT_FOUND_REPROMPT");
+            let speechOutput = this.t('RECIPE_NOT_FOUND_MESSAGE');
+            const repromptSpeech = this.t('RECIPE_NOT_FOUND_REPROMPT');
             if (itemName) {
-                speechOutput += this.t("RECIPE_NOT_FOUND_WITH_ITEM_NAME", itemName);
+                speechOutput += this.t('RECIPE_NOT_FOUND_WITH_ITEM_NAME', itemName);
             } else {
-                speechOutput += this.t("RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME");
+                speechOutput += this.t('RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME');
             }
             speechOutput += repromptSpeech;
 
-            this.attributes['speechOutput'] = speechOutput;
-            this.attributes['repromptSpeech'] = repromptSpeech;
+            this.attributes.speechOutput = speechOutput;
+            this.attributes.repromptSpeech = repromptSpeech;
 
             this.emit(':ask', speechOutput, repromptSpeech);
         }
     },
     'AMAZON.HelpIntent': function () {
-        this.attributes['speechOutput'] = this.t("HELP_MESSAGE");
-        this.attributes['repromptSpeech'] = this.t("HELP_REPROMPT");
-        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+        this.attributes.speechOutput = this.t('HELP_MESSAGE');
+        this.attributes.repromptSpeech = this.t('HELP_REPROMPT');
+        this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
     },
     'AMAZON.RepeatIntent': function () {
-        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+        this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
     },
     'AMAZON.StopIntent': function () {
         this.emit('SessionEndedRequest');
@@ -68,38 +96,21 @@ var handlers = {
     'AMAZON.CancelIntent': function () {
         this.emit('SessionEndedRequest');
     },
-    'SessionEndedRequest':function () {
-        this.emit(':tell', this.t("STOP_MESSAGE"));
+    'SessionEndedRequest': function () {
+        this.emit(':tell', this.t('STOP_MESSAGE'));
     },
     'Unhandled': function () {
-        this.attributes['speechOutput'] = this.t("HELP_MESSAGE");
-        this.attributes['repromptSpeech'] = this.t("HELP_REPROMPT");
-        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
-    }
+        this.attributes.speechOutput = this.t('HELP_MESSAGE');
+        this.attributes.repromptSpeech = this.t('HELP_REPROMPT');
+        this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
+    },
 };
 
-var languageStrings = {
-    "en": {
-        "translation": {
-            "RECIPES": recipes.RECIPE_EN_US,
-            "SKILL_NAME": "Terraria Crafting Tool",
-            "WELCOME_MESSAGE": "Welcome to %s. You can ask a question like, what\'s the recipe for a torch? ... So, what can I help you craft.",
-            "WELCOME_REPROMPT": "For instructions on what you can say, please say help me.",
-            "DISPLAY_CARD_TITLE": "%s  - Recipe for %s.",
-            "HELP_MESSAGE": "You can ask questions such as, what\'s the recipe, or, you can say exit...So, what can I help you with?",
-            "HELP_REPROMPT": "You can say things like, what\'s the recipe, or you can say exit...So how can I help you?",
-            "STOP_MESSAGE": "Goodbye!",
-            "RECIPE_REPEAT_MESSAGE": "Try saying repeat.",
-            "RECIPE_NOT_FOUND_MESSAGE": "I\'m sorry, I currently do not know, try back later ",
-            "RECIPE_NOT_FOUND_WITH_ITEM_NAME": "the recipe for %s. ",
-            "RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME": "that recipe. ",
-            "RECIPE_NOT_FOUND_REPROMPT": "What else can I help with?"
-        }
-    },
-    "en-US": {
-        "translation": {
-            "RECIPES" : recipes.RECIPE_EN_US,
-            "SKILL_NAME" : "American Terraria Crafting Tool"
-        }
-    }
+exports.handler = function (event, context) {
+    const alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    // To enable string internationalization (i18n) features, set a resources object.
+    alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
 };
