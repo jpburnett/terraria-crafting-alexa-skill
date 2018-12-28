7a4d078301839323eff9ec802a00de6351c4ca03
Parker Burnett <burnettparker@gmail.com>
2018-12-27 22:03:27 -0700

    Changed some stuff for testing. Put some things in external file.

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -3,6 +3,7 @@
 
 const Alexa = require('ask-sdk-core');
 const recipes = require('./recipes');
+const languageStrings = require('./languageStrings').languageStrings
 const i18n = require('i18next');
 const sprintf = require('i18next-sprintf-postprocessor');
 
@@ -15,6 +16,7 @@ const LaunchRequestHandler = {
         return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
     },
     handle(handlerInput) {
+        console.log("In LaunchRequestHandler");
         const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
         const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
 
@@ -24,6 +26,7 @@ const LaunchRequestHandler = {
         const repromptOutput = requestAttributes.t('WELCOME_REPROMPT');
 
         handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -155,10 +158,10 @@ const ErrorHandler = {
     },
     handle(handlerInput, error) {
         console.log(`Error handled: ${error.message}`);
-
+        
         return handlerInput.responseBuilder
-            .speak('Sorry, I can\'t understand the command. Please say again.')
-            .reprompt('Sorry, I can\'t understand the command. Please say again.')
+            .speak('There was an Error.')
+            .reprompt('Sorry, There was an error.')
             .getResponse();
     },
 };
@@ -168,39 +171,21 @@ const ErrorHandler = {
 //////////////////////////////////////////////////////////////////////////////
 
 const skillBuilder = Alexa.SkillBuilders.custom();
-const languageStrings = {
-    en: {
-        translation: {
-            RECIPES: recipes.RECIPE_EN_US,
-            SKILL_NAME: 'Terraria Tool',
-            WELCOME_MESSAGE: "Welcome to %s. You can ask a question like, what\'s the recipe for a torch? ... Now, how may I help you?",
-            WELCOME_REPROMPT: 'For instructions on what you can say, please say help me.',
-            DISPLAY_CARD_TITLE: '%s  - Recipe for %s.',
-            HELP_MESSAGE: "You can ask questions such as, what\'s the recipe, or, you can say exit...Now, how may I help you?",
-            HELP_REPROMPT: "You can say things like, what\'s the recipe, or you can say exit...Now, how can I help?",
-            STOP_MESSAGE: 'Happy Crafting!',
-            RECIPE_REPEAT_MESSAGE: 'Try saying repeat.',
-            RECIPE_NOT_FOUND_MESSAGE: "I\'m sorry, I don\'t know, ",
-            RECIPE_NOT_FOUND_WITH_ITEM_NAME: ' the recipe for %s. ',
-            RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME: ' that recipe. ',
-            RECIPE_NOT_FOUND_REPROMPT: 'How else can I help?',
-        },
-    },
-    'en-US': {
-        translation: {
-            RECIPES: recipes.RECIPE_EN_US,
-            SKILL_NAME: 'Terraria Tool'
-        },
-    },
-    'en-GB': {
-        translation: {
-            RECIPES: recipes.RECIPE_EN_GB,
-            SKILL_NAME: 'British Terraria Tool'
-        },
-    },
-};
+// const languageStrings = {
+//     'en-US': {
+//         translation: {
+//             RECIPES: recipes.RECIPE_EN_US,
+//             SKILL_NAME: 'Terraria Tool'
+//         },
+//     },
+//     'en-GB': {
+//         translation: {
+//             RECIPES: recipes.RECIPE_EN_GB,
+//             SKILL_NAME: 'British Terraria Tool'
+//         },
+//     },
+// };
 
-// Finding the locale of the user (IDK what I'll use it for...)
 const LocalizationInterceptor = {
     process(handlerInput) {
         const localizationClient = i18n.use(sprintf).init({
---
+++ lambda/custom/languageStrings.js
@@ -0,0 +1,38 @@
+module.exports = {
+    languageStrings: {
+        en: {
+            translation: {
+                RECIPES: 'recipes.RECIPE_EN_US',
+                'SKILL_NAME': 'Terraria Tool',
+                'WELCOME_MESSAGE': "Welcome to %s. You can ask a question like, what\'s the recipe for a torch? ... Now, how may I help you?",
+                'WELCOME_REPROMPT': 'For instructions on what you can say, please say help me.',
+                DISPLAY_CARD_TITLE: '%s  - Recipe for %s.',
+                HELP_MESSAGE: "You can ask questions such as, what\'s the recipe, or, you can say exit...Now, how may I help you?",
+                HELP_REPROMPT: "You can say things like, what\'s the recipe, or you can say exit...Now, how can I help?",
+                STOP_MESSAGE: 'Happy Crafting!',
+                RECIPE_REPEAT_MESSAGE: 'Try saying repeat.',
+                RECIPE_NOT_FOUND_MESSAGE: "I\'m sorry, I don\'t know, ",
+                RECIPE_NOT_FOUND_WITH_ITEM_NAME: ' the recipe for %s. ',
+                RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME: ' that recipe. ',
+                RECIPE_NOT_FOUND_REPROMPT: 'How else can I help?',
+            }
+        },
+        'en-US': {
+            translation: {
+                RECIPES: 'recipes.RECIPE_EN_US',
+                SKILL_NAME: 'Terraria Tool',
+                WELCOME_MESSAGE: "Welcome to %s. You can ask a question like, what\'s the recipe for a torch? ... Now, how may I help you?",
+                WELCOME_REPROMPT: 'For instructions on what you can say, please say help me.',
+                DISPLAY_CARD_TITLE: '%s  - Recipe for %s.',
+                HELP_MESSAGE: "You can ask questions such as, what\'s the recipe, or, you can say exit...Now, how may I help you?",
+                HELP_REPROMPT: "You can say things like, what\'s the recipe, or you can say exit...Now, how can I help?",
+                STOP_MESSAGE: 'Happy Crafting!',
+                RECIPE_REPEAT_MESSAGE: 'Try saying repeat.',
+                RECIPE_NOT_FOUND_MESSAGE: "I\'m sorry, I don\'t know, ",
+                RECIPE_NOT_FOUND_WITH_ITEM_NAME: ' the recipe for %s. ',
+                RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME: ' that recipe. ',
+                RECIPE_NOT_FOUND_REPROMPT: 'How else can I help?',
+            }
+        }
+    }
+};
